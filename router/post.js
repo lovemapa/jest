89bb9808cf7999d2dab35b3d7413abdd58b11c59
Pawan Kumar <kumar16.pawan@gmail.com>
2021-09-08 23:47:00 +0530

    added comment

--- router/post.js
+++ router/post.js
@@ -4,6 +4,7 @@ const Posts = require('../controllers/posts')
 const post = new Posts()
 const postRouter = express.Router()
 
+//Routes
 postRouter.post('/', post.createPost)
 postRouter.get('/', post.getPosts)
 
